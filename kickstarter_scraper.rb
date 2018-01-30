4b47f2df310430792b86e29a1009e7af3e0b1fb7
Robert Hughes <roberts.ginger.email@gmail.com>
2018-01-30 19:22:59 +0000

    automatically backed up by learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -2,9 +2,9 @@ require 'nokogiri'
 require 'pry'
 
 def create_project_hash
-  html = File.read('fixtures/kickstarter.html')
+  html = File.read("fixtures/kickstarter.html")
   kickstarter = Nokogiri::HTML(html)
-  project = kickstarter.css('li.project.grid_4')
+  project = kickstarter.css("li.project.grid_4")
   title = project.css("h2.bbcard_name strong a").text
   image_link = project.css("div.project-thumbnail a img").attribute("src").value
   description = project.css("p.bbcard_blurb").text
